e469d27c1f02791ed8c2fc61cedaab1b0fbb725f
Sheldonccf <89536920+Sheldonccf@users.noreply.github.com>
2021-11-19 07:36:52 +0000

    Add files via upload

---
+++ CppND-System-Monitor/CppND-System-Monitor/CppND-System-Monitor/include/processor.h
@@ -0,0 +1,24 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+class Processor {
+ public:
+  float Utilization();  // TODO: See src/processor.cpp
+
+  // TODO: Declare any necessary private members
+ private:
+  long int user;
+  long int nice;
+  long int system;
+  long int idle;
+  long int iowait;
+  long int irq;
+  long int softirq;
+  long int steal;
+  long int guest;
+  long int guest_nice;
+  long int PrevTotal;
+  long int PrevIdle;
+};
+
+#endif
