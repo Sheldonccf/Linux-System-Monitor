80d171400c5f66a6cd9edf864cb8dce190da8cbb
Sheldonccf <89536920+Sheldonccf@users.noreply.github.com>
2022-01-09 08:11:13 -0500

    Update system.cpp
    Delete Comments

--- CppND-System-Monitor/src/system.cpp
+++ CppND-System-Monitor/src/system.cpp
@@ -14,14 +14,10 @@ using std::size_t;
 using std::string;
 using std::vector;
 using namespace LinuxParser;
-/*You need to complete the mentioned TODOs in order to satisfy the rubric criteria "The student will be able to extract and display basic data about the system."
 
-You need to properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
-
-// TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a containProcessor::Utilization()er composed of the system's processes
+
 vector<Process>& System::Processes() { 
     vector<int> PidList = LinuxParser::Pids();
     
@@ -40,24 +36,18 @@ vector<Process>& System::Processes() {
 return *processPtr;
  }
 
-// TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { 
    return LinuxParser::Kernel();
     }
 
-// TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// TODO: Return the operating system name
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
-// TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
-// TODO: Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
-// TODO: Return the number of seconds since the system started running
 long int System::UpTime() { 
     return LinuxParser::UpTime();  
     }
