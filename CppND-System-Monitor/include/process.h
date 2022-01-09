a6e9e2e2488b474c256bedcf192bc839946d17de
Sheldonccf <89536920+Sheldonccf@users.noreply.github.com>
2022-01-09 08:09:10 -0500

    Update process.h
    Delete comments

--- CppND-System-Monitor/include/process.h
+++ CppND-System-Monitor/include/process.h
@@ -9,15 +9,14 @@ It contains relevant attributes as shown below
 class Process {
  public:
   Process(int pid): Pid_(pid) {};
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  int Pid();                               
+  std::string User();                      
+  std::string Command();                   
+  float CpuUtilization();                  
+  std::string Ram();                       
+  long int UpTime();                       
+  bool operator<(Process const& a) const;  
 
-  // TODO: Declare any necessary private members
  private:
    int Pid_;
    std::string User_;
