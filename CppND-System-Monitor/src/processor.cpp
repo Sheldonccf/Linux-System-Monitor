945cce4781fea7c383beb219d791ad769dde3333
Sheldonccf <89536920+Sheldonccf@users.noreply.github.com>
2022-01-09 08:12:22 -0500

    Update processor.cpp
    delete changes

--- CppND-System-Monitor/src/processor.cpp
+++ CppND-System-Monitor/src/processor.cpp
@@ -12,14 +12,12 @@ using std::string;
 using std::to_string;
 using std::vector;
 using std::cout;
-// TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { 
 
     vector<string> cpuUtlis = LinuxParser::CpuUtilization();
     //parse linux files to get values for current system usage by type
      
      Processor::user = std::stol(cpuUtlis[0], nullptr);
-     //std::cout << Processor::user << "\n";
      Processor::nice = std::stol(cpuUtlis[1], nullptr);
      Processor::system = std::stol(cpuUtlis[2], nullptr);
      Processor::idle = std::stol(cpuUtlis[3], nullptr);
@@ -38,19 +36,14 @@ float Processor::Utilization() {
 
      long Total = Idle + NoIdle;
 
-     //std::cout << Total << "\n";
-
      long changeIdle = Idle - Processor::PrevIdle;
      long changeTotal = Total - Processor::PrevTotal;
-     //std::cout << changeIdle << "\n";
-     //std::cout << changeTotal << "\n";
 
      float Cpu_percentage = (float(changeTotal)-changeIdle)/float(changeTotal);
-     //std::cout << Cpu_percentage << "\n";
+
     // save current total and Idle usage as Prev values
      Processor::PrevIdle = Idle;
      Processor::PrevTotal = Total;
 
-     //std::cout << Cpu_percentage << "\n";
      return Cpu_percentage;
     }
